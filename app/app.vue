028a5b0c4ff4d59077587ef838a4428097c45781
ermolaevaalenaandreevna <alyonaerm13@gmail.com>
2025-12-08 16:12:08 +1000

    Add lab3

---
+++ app/app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Header />
+    <Banner />
+    <main class="main">
+      <h1>Hello, World!</h1>
+      <p>Look at my website</p>
+      <div class="cards">
+        <Card text="Here" />
+        <Card text="And here" />
+      </div>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<style lang="scss">
+@use '~/assets/scss/mixins' as *;
+
+.main {
+  padding: 24px;
+  text-align: center;
+
+  .cards {
+    display: flex;
+    justify-content: center;
+    gap: 16px;
+    margin-top: 16px;
+  }
+}
+</style>
+<script setup lang="ts">
+import Footer from "~/components/Footer.vue";
+
+</script>
