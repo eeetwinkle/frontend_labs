be515e31a1a11ad7fafee6bf6fe04f073ddaf2f0
ermolaevaalenaandreevna <alyonaerm13@gmail.com>
2026-01-29 20:48:31 +1000

    add lab8

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,13 @@
     <main class="main">
       <Slider />
       <Description />
+      <section class="projects-section">
+        <div class="projects-container">
+          <h2 class="section-title">Реализованные проекты</h2>
+          <ProjectsList />
+        </div>
+      </section>
+      <NewsList />
     </main>
     <Footer />
   </div>
@@ -11,6 +18,9 @@
 
 <style lang="scss">
 @use 'reset-css';
+@use "~/assets/scss/variables" as vars;
+@use "~/assets/scss/mixins" as mixins;
+
 
 .swiper {
   width: 100%;
@@ -30,17 +40,77 @@
   height: 100%;
   flex-shrink: 0;
 }
-
 .layout {
   display: grid;
   grid-template-rows: auto 1fr auto;
   min-height: 100vh;
+  width: 100%;
+  max-width: 100vw; /* Ограничиваем ширину */
+  overflow-x: hidden; /* Скрываем горизонтальный скролл */
 }
 
 .main {
   padding: 24px;
   text-align: center;
+  width: 100%;
+  overflow-x: hidden; /* Важно! */
+
+  @include mixins.small {
+    padding: 16px; /* Уменьшаем padding на мобилках */
+  }
 }
+
+
+.projects-section {
+  background-color: vars.$color-light-green;
+  padding-bottom: 100px;
+
+  position: relative;
+  width: 105vw;
+  left: 50%;
+  right: 50%;
+  margin-left: -54vw;
+  margin-right: -50vw;
+  overflow: auto; /* Изменяем overflow */
+  box-sizing: content-box;
+
+  @include mixins.small {
+    padding-bottom: 60px;
+    padding-top: 40px;
+    margin-top: 40px;
+    margin-right: 10px;
+    margin-bottom: 40px;
+  }
+}
+
+
+.projects-container {
+  margin-left: 88px;
+  margin-right: 88px;
+  margin-top: 40px;
+
+  @include mixins.small {
+    margin-left: 16px;
+    margin-right: 16px;
+    margin-top: 16px;
+    width: calc(100% - 32px); /* Вычитаем margin */
+  }
+}
+
+.section-title {
+  font-family: vars.$font-title;
+  font-weight: 700;
+  font-size: 36px;
+  margin: 0 0 40px 0;
+  text-align: left;
+  color: vars.$color-black;
+
+  @include mixins.small {
+    font-size: 24px;
+    margin-bottom: 24px;
+  }
+}
+
 </style>
 
 <script setup lang="ts">
@@ -48,4 +118,5 @@ import Header from "~/components/Header.vue";
 import Footer from "~/components/Footer.vue";
 import Slider from "~/components/Slider.vue";
 import Description from "~/components/Description.vue";
+import ProjectsList from "~/components/ProjectsList.vue";
 </script>
